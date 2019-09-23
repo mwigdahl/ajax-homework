ffaa5eb42f7f4d7bfe80a5e4db0a5b7fc557e734
Matt Wigdahl <matt@leverpulley.com>
2019-09-23 12:02:42 -0600

    playing around with functions

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,8 @@
 var animals = ['cats', 'dogs', 'birds', 'horses', 'chickens', 'mouse', 'pigs', 'fish', 'giraffes', 'lions'];
-var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=GIrOZj09NaiRq20vuzsB3xRXw9YbLo5J&limit=10&rating=g&q=horses'; 
+
 
 function displayGiphy() {
-
+    var queryURL = 'https://api.giphy.com/v1/gifs/search?api_key=GIrOZj09NaiRq20vuzsB3xRXw9YbLo5J&limit=10&rating=g&q=' + myGiphy; 
 
 $.ajax({
  url: queryURL,
@@ -10,33 +10,42 @@ $.ajax({
 }).then(function(response) { 
 var result = response.data;
     console.log(response.data);
-
-    for (var j = 0; j < result.length; j++) {
-
-    var giphy = $('<img>');
-    giphy.attr('data-name', 'animate');
-    giphy.attr('src', result[j].images.fixed_width.url);
-    giphy.text(result[j].rating);
-    $('#show-giphy').append(giphy);
+    $('#add-giphy').on('click', function(event) {
+        event.preventDefault();
     
-    }
+        var myGiphy = $('#myGiphy').val().trim();
+    
+        animals.push(myGiphy);
+    
+    });
 
 });
+
+function createGiphyList() {
+    for (var j = 0; j < result.length; j++) {
+
+        var giphy = $('<img>');
+        giphy.attr('data-name', 'animate');
+        giphy.attr('src', result[j].images.fixed_width.url);
+        giphy.text(result[j].rating);
+        $('#show-giphy').append(giphy);
+        
+        }
+}
+
+createGiphyList();
 
 
 }
-//displayGiphy();
 
-$('#add-giphy').on('click', function(event) {
-    event.preventDefault();
 
-    var myGiphy = $('#myGiphy').val().trim();
+    renderButtons();
 
-    //myGiphy.push();
 
-});
 
 function renderButtons() {
+    
+    $('#show-buttons').empty();
 
     for (var i = 0; i < animals.length; i++) {
     
@@ -45,11 +54,13 @@ function renderButtons() {
         animalBtn.attr('data-name', animals[i]);
         animalBtn.text(animals[i]);
         $('#show-buttons').append(animalBtn);
-
+    
     }
+    
 
 }
-renderButtons();
+
+$(document).on('click', 'animal-btn', displayGiphy);
 
 
 // https://api.giphy.com/v1/gifs/search
